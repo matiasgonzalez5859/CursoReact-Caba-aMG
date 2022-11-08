ba4cf4317eced6935a2eb83328c4b32218d5cbaa
matias gonzalez <matias5859@gmail.com>
2022-11-08 18:39:42 -0300

    database

---
+++ src/Data/Data.js
@@ -0,0 +1,61 @@
+const products = [
+    {
+      id: 1,
+      title: "Terneras Braford",
+      price: 700,
+      stock: 49,
+      category: "Terneras carne",
+      imgurl: "./img/Braford-.jpg",
+      description: "An apple mobile which is nothing like apple",
+    },
+    {
+      id: 2,
+      title: "Terneras Brahman",
+      description:
+        "Vaquillonas lote 950",
+      price: 899,
+      stock: 34,
+      category: "Terneras carne",
+      imgurl: "/img/brahman.jpg",
+    },
+    {
+      id: 4,
+      title: "Teneras Brangus Coloradas",
+      description: "Vaquillonas Brangus Coloradas lote 548",
+      price: 280,
+      stock: 123,
+      category: "Terneras Carne",
+      imgurl: "/img/branguscolos.jpg",
+    },
+    {
+      id: 5,
+      title: "Brangus Overas",
+      description:
+        "Lote 854",
+      price: 499,
+      stock: 32,
+      category: "Vaquillona de carne ",
+      imgurl: "/img/brangusovero.jpg",
+    },
+    {
+      id: 6,
+      title: "Vaquillona criolla",
+      description:
+        "551 ",
+      price: 1749,
+      stock: 83,
+      category: "Vaquillona de carne",
+      imgurl: "/img/criollas.jpg",
+    },
+    {
+      id: 7,
+      title: "Vaquillona Holandas",
+      description:
+        "478",
+      price: 1499,
+      stock: 50,
+      category: "vaquillonas de leche",
+      imgurl: "/img/holanda.jpg",
+    },
+]
+export default products;
--- src/ItemList/ItemListContainer.jsx
+++ src/ItemList/ItemListContainer.jsx
@@ -1,29 +1,33 @@
-
-
-
 import React from 'react'
 import Item from './Item'
 import "./ItemList.css"
+import products from "../Data/Data"
 
-function ItemListContainer(props) {
+function ItemListContainer() {
   
-
-  
-
-  
-
   return (
     <div className="item-list">
-        <h1>{props.greeting}</h1>
-        
-        <Item imgurl="/img/Braford-.jpg" title="Braford" price= {100} color="fireback" />
-        <Item imgurl="/img/shorton.jpg"  title="Vaquillonas Shorton" price= {100} color="purple" />
-        <Item imgurl="/img/holanda.jpg" title="Vaquillonas Holandas" price= {100} color="darkgreen" />
-        <Item imgurl="/img/holanda.jpg" title="Vaquillonas Holandas" price= {100} color="darkgreen" />
-        <Item imgurl="/img/holanda.jpg" title="Vaquillonas Holandas" price= {100} color="darkgreen" />
-        <Item imgurl="/img/holanda.jpg" title="Vaquillonas Holandas" price= {100} color="darkgreen" />
+      {products.map((product) => {
+        return (
+          <Item
+            key={product.id}
+            imgurl={product.imgurl}
+            title={product.title}
+            price={product.price}
+            category={product.category}
+            color="darkgreen"
+          />
+        );
+      })}
     </div>
-  )
+  );
 }
+
+
+
+  
+
+
+
 
 export default ItemListContainer
